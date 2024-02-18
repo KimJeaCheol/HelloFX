0995f07f5aa0950d1703f9706a80e7d78cce1405
KimJeaCheol <cresh0105@gmail.com>
2024-02-18 17:21:45 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		stage.setScene(scene);
+		Image icon = new Image("icon.png");
+		stage.getIcons().add(icon);
+		stage.setTitle("게시판");
+		stage.setResizable(false);
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/Service/BoardServiceImpl.java
@@ -0,0 +1,47 @@
+package Service;
+
+import java.util.List;
+
+import DAO.BoardDAO;
+import DTO.Board;
+
+public class BoardServiceImpl implements BoardService{
+
+	private BoardDAO boardDao = new BoardDAO();
+	
+	@Override
+	public List<Board> list() {
+		// TODO Auto-generated method stub
+		List<Board> boardList = boardDao.list();
+		return boardList;
+	}
+
+	@Override
+	public Board select(int boardNo) {
+		// TODO Auto-generated method stub
+		Board board = boardDao.select(boardNo);
+		return board;
+	}
+
+	@Override
+	public int insert(Board board) {
+		// TODO Auto-generated method stub
+		int result = boardDao.insert(board);
+		return result;
+	}
+
+	@Override
+	public int update(Board board) {
+		// TODO Auto-generated method stub
+		int result = boardDao.update(board);
+		return result;
+	}
+
+	@Override
+	public int delete(int boardNo) {
+		// TODO Auto-generated method stub
+		int result = boardDao.delete(boardNo);
+		return result;
+	}
+	
+}
