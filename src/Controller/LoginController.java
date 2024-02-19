f6501ae5170c97d21f929c8e7fab33f8fda339f8
KimJeaCheol <cresh0105@gmail.com>
2024-02-19 23:43:30 +0900

    로그인,대시보드  개발

---
+++ src/Controller/LoginController.java
@@ -0,0 +1,17 @@
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+public class LoginController implements Initializable {
+
+    Stage stage;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+      
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -10,14 +9,11 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("/UI/Dashboard.fxml"));
 		Scene scene = new Scene(root);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		stage.setScene(scene);
-		Image icon = new Image("icon.png");
-		stage.getIcons().add(icon);
-		stage.setTitle("게시판");
-		stage.setResizable(false);
+	    //stage.initStyle(StageStyle.UNDECORATED);//타이틀바 삭제
 		stage.show();
 	}
 	
