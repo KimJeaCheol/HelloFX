e4e994ce3797cc8579e086a2ff5ccae9518b3ff4
KimJeaCheol <cresh0105@gmail.com>
2024-02-20 23:16:51 +0900

    대시보드 개발

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application{
 	
+	double x, y = 0;
 
 	@Override
 	public void start(Stage stage) throws Exception {
+
 		Parent root = FXMLLoader.load(getClass().getResource("/UI/Dashboard.fxml"));
-		Scene scene = new Scene(root);
-		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		stage.initStyle(StageStyle.UNDECORATED);//타이틀바 삭제
+		
+		root.setOnMousePressed(event -> {
+			x = event.getSceneX();
+			y = event.getScreenY();
+		});
+
+		root.setOnMouseDragged(event -> {
+			stage.setX(event.getScreenX() - x);
+			stage.setY(event.getScreenY() - y);
+		});
+
+		Scene scene = new Scene(root,800,500);
+		
 		stage.setScene(scene);
-	    //stage.initStyle(StageStyle.UNDECORATED);//타이틀바 삭제
 		stage.show();
 	}
 	
